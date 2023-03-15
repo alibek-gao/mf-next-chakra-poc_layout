100efd2632586fbde3a6907ee18c8fd074180ab7
Alibek Kazhibekov <alibek.gao@gmail.com>
2023-03-15 16:41:42 +0100

    fix: replace custom font

--- components/hero.js
+++ components/hero.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
 import Link from 'next/link';
+import { Caveat } from 'next/font/google'
 import {
   Box,
   Heading,
@@ -8,9 +8,13 @@ import {
   Button,
   Stack,
   Icon,
-  useColorModeValue,
   createIcon,
 } from '@chakra-ui/react';
+
+const caveat = Caveat({
+  subsets: ['latin'],
+  weight: '700',
+});
 
 const Arrow = createIcon({
   displayName: 'Arrow',
@@ -27,79 +31,71 @@ const Arrow = createIcon({
 
 export default function Hero() {
   return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Container maxW={'3xl'}>
-        <Stack
-          as={Box}
-          textAlign={'center'}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}>
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-            Make money from <br />
-            <Text as={'span'} color={'green.400'}>
-              your audience
-            </Text>
-          </Heading>
-          <Text color={'gray.500'}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
+    <Container maxW={'3xl'}>
+      <Stack
+        as={Box}
+        textAlign={'center'}
+        spacing={{ base: 8, md: 14 }}
+        py={{ base: 20, md: 36 }}>
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+          lineHeight={'110%'}>
+          Make money from <br />
+          <Text as={'span'} color={'green.400'}>
+            your audience
           </Text>
-          <Stack
-            direction={'column'}
-            spacing={3}
-            align={'center'}
-            alignSelf={'center'}
-            position={'relative'}>
-            <Link href='/features'>
-              <Button
-                colorScheme={'green'}
-                bg={'green.400'}
-                rounded={'full'}
-                px={6}
-                _hover={{
-                  bg: 'green.500',
-                }}>
-                Features
-              </Button>
-            </Link>
-            <Link href='/pricing'>
-              <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
-                Pricing
-              </Button>
-            </Link>
-            <Box>
-              <Icon
-                as={Arrow}
-                color={useColorModeValue('gray.800', 'gray.300')}
-                w={71}
-                position={'absolute'}
-                right={-71}
-                top={'10px'}
-              />
-              <Text
-                fontSize={'lg'}
-                fontFamily={'Caveat'}
-                position={'absolute'}
-                right={'-125px'}
-                top={'-15px'}
-                transform={'rotate(10deg)'}>
-                Starting at $15/mo
-              </Text>
-            </Box>
-          </Stack>
+        </Heading>
+        <Text color={'gray.500'}>
+          Monetize your content by charging your most loyal readers and reward
+          them loyalty points. Give back to your loyal readers by granting
+          them access to your pre-releases and sneak-peaks.
+        </Text>
+        <Stack
+          direction={'column'}
+          spacing={3}
+          align={'center'}
+          alignSelf={'center'}
+          position={'relative'}>
+          <Link href='/features'>
+            <Button
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}>
+              Features
+            </Button>
+          </Link>
+          <Link href='/pricing'>
+            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+              Pricing
+            </Button>
+          </Link>
+          <Box>
+            <Icon
+              as={Arrow}
+              color={'gray.800'}
+              w={71}
+              position={'absolute'}
+              right={-71}
+              top={'10px'}
+            />
+            <Text
+              className={caveat.className}
+              fontSize={'lg'}
+              fontFamily={'Caveat'}
+              position={'absolute'}
+              right={'-125px'}
+              top={'-15px'}
+              transform={'rotate(10deg)'}>
+              Starting at $15/mo
+            </Text>
+          </Box>
         </Stack>
-      </Container>
-    </>
+      </Stack>
+    </Container>
   );
 }
