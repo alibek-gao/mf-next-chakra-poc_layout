b3a82a49d7282610972d7f6ad415593b8abb686b
Alibek Kazhibekov <alibek.gao@gmail.com>
2023-03-15 22:25:41 +0100

    feat: add chakra colorModeManager

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,29 @@
 import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, cookieStorageManagerSSR } from '@chakra-ui/react'
 
 import Nav from '@/components/nav'
-export default function App({ Component, pageProps }) {
+
+const theme = {
+  config: {
+    initialColorMode: 'dark',
+  }
+}
+export default function App({ Component, pageProps, cookies }) {
+  const colorModeManager = cookieStorageManagerSSR(cookies);
+
   return (
     <CacheProvider>
-      <ChakraProvider>
+      <ChakraProvider colorModeManager={colorModeManager} theme={theme}>
         <Nav />
         <Component {...pageProps} />
       </ChakraProvider>
     </CacheProvider>
   )
 }
+
+App.getInitialProps = (data) => {
+  const { ctx } = data;
+  return {
+    cookies: ctx?.req?.headers?.cookie || '',
+  };
+};
