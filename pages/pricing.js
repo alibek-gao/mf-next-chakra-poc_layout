43e30eea7b1b00dc1e5aed9dc9af3f9a4a2c0692
Alibek Kazhibekov <alibek.gao@gmail.com>
2023-04-24 15:41:17 +0200

    feat: change pricing

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,49 +1,19 @@
 import dynamic from 'next/dynamic';
 
 const PricingComponent = dynamic(
-  () => import('content/Pricing').then((mod) => mod.Pricing),
+  () => import('content/Pricing'),
   {
     ssr: true,
   }
 );
-
-const ListComponent = dynamic(
-  () => import('content/List').then((mod) => mod.List),
-  {
-    ssr: true,
-  }
-);
-export function Pricing({ pricingProps, listProps }) {
-  return (
-    <>
-      <PricingComponent {...pricingProps} />
-      <ListComponent {...listProps} />
-    </>
-  )
+export function Pricing({ ...props }) {
+  return <PricingComponent {...props} />
 }
 
 export const getServerSideProps = async (ctx) => {
-  const [pricing, list] = await Promise.all([
-    import('content/Pricing'),
-    import('content/List'),
-  ]);
+  const pricing = await import('content/Pricing')
 
-  if (pricing.getServerSideProps && list.getServerSideProps) {
-    const [pricingProps, listProps] = await Promise.all([
-      pricing.getServerSideProps(ctx),
-      list.getServerSideProps(ctx),
-    ]);
-    return {
-      props: {
-        pricingProps: pricingProps.props,
-        listProps: listProps.props,
-      }
-    }
-  }
-
-  return {
-    props: {},
-  }
+  return pricing.getServerSideProps(ctx)
 }
 
 export default Pricing;
